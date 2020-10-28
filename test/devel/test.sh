598ad84c6e8f411255fbed317238468d63c8232b
charles-korpics <charles.korpics@ibm.com>
2020-10-27 23:15:27 -0400

    testing

---
+++ test/devel/test.sh
@@ -0,0 +1,19 @@
+set -e
+
+export ANSI_YELLOW="\e[1;33m"
+export ANSI_GREEN="\e[32m"
+export ANSI_RESET="\e[0m"
+
+echo -e "\n $ANSI_YELLOW *** testing docker run - bash *** $ANSI_RESET \n"
+
+echo -e "$ANSI_YELLOW Test docker run of Bash: $ANSI_RESET"
+docker run -i --name some-bash quay.io/ibmz/bash:devel bash --version
+docker inspect some-bash | grep 'Running'
+
+# expecting
+# Hello World!
+docker exec -i some-bash echo 'Hello World!'
+
+docker rm some-bash
+
+echo -e "\n $ANSI_GREEN *** TEST COMPLETED SUCESSFULLY *** $ANSI_RESET \n"
