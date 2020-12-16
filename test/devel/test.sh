94ef25981854c08dad92eeacd9f1084c3c93ceb8
Charles Korpics <72992136+korpx-z@users.noreply.github.com>
2020-12-16 12:40:57 -0500

    Update test.sh

--- test/devel/test.sh
+++ test/devel/test.sh
@@ -7,10 +7,10 @@ export ANSI_RESET="\e[0m"
 echo -e "\n $ANSI_YELLOW *** testing docker run - bash *** $ANSI_RESET \n"
 
 echo -e "$ANSI_YELLOW Test docker run of Bash: $ANSI_RESET"
-docker run -i --rm --name version-bash quay.io/ibmz/bash:devel bash --version
+docker run -i --rm --name version-bash quay.io/ibm/bash:devel bash --version
 
 # expecting
 # Hello World!
-docker run -i --rm --name hello-bash quay.io/ibmz/bash:devel echo 'Hello World!'
+docker run -i --rm --name hello-bash quay.io/ibm/bash:devel echo 'Hello World!'
 
 echo -e "\n $ANSI_GREEN *** TEST COMPLETED SUCESSFULLY *** $ANSI_RESET \n"
